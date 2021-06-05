b300fc6c6cd56b6a2a8867f06efa1fdf1e0b19ca
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-06-05 13:08:07 -0400

    Full rebuild.

--- 2_build_docs.py
+++ 2_build_docs.py
@@ -1,5 +1,5 @@
 import common
 
 # Args are SOURCEDIR OUTPUTDIR FILENAMES
-common.require_command(f'{common.sphinx_exe} source docs -d cached'.split(),
+common.require_command(f'{common.sphinx_exe} source docs -d cached -E'.split(),
     'Unable to build docs with sphinx')
