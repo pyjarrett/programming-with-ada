450b3dd9550f6c097ae957e677f56c6a9833d045
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-06-05 12:13:09 -0400

    Remove unused pathlib import.

--- 2_build_docs.py
+++ 2_build_docs.py
@@ -1,7 +1,5 @@
 import common
 
-import pathlib
-
 # Args are SOURCEDIR OUTPUTDIR FILENAMES
 common.require_command(f'{common.sphinx_exe} source docs -d cached'.split(),
     'Unable to build docs with sphinx')
